d2dd70874ada05feb468111e8a5db1f1a91c32d7
Sebastian LopezCot <slcot>
2018-08-18 14:55:52 -0400

    Simplifying data structure to ring buffer only

--- bpq.go
+++ bpq.go
@@ -1,7 +1,9 @@
 package bpq
 
 import (
+  "encoding/json"
   "errors"
+  "fmt"
 )
 
 type QueueType int
@@ -9,6 +11,12 @@ type QueueType int
 type QueueItem struct {
   Value    interface{}
   Priority float64
+}
+
+type QueueMarshalled struct {
+  Items    []QueueItem
+  Capacity int
+  Ordering QueueType
 }
 
 const (
@@ -25,20 +33,21 @@ func (queueType QueueType) String() string {
   return queueTypes[queueType-1]
 }
 
-// BPQ represents a bounded min-priority queue
-type BPQ interface {
-  // Capacity returns the capacity (bounds) on the underlying queue
-  Capacity() int
-  // Push attempts to enqueue an item to the queue, with the given priority
-  // Returns whether it succeded or not (failure is only due to a full queue,
-  // where all items are lower priority)
-  Push(interface{}, float64) bool
-  // Pop attempts to pop an item from the queue. It returns the popped item,
-  // or an error. The only error it returns is NoElementsError, indicating
-  // that the queue is empty
-  Pop() (QueueItem, error)
-  // QueueType returns the type of the underlying queue
-  QueueType() QueueType
+func (queueType QueueType) MarshalJSON() ([]byte, error) {
+  return json.Marshal(queueType.String())
+}
+
+func (queueType *QueueType) UnmarshalJSON(text []byte) error {
+  textStr := string(text)
+
+  for i, typeString := range queueTypes {
+    if "\""+typeString+"\"" == textStr {
+      *queueType = QueueType(i + 1)
+      return nil
+    }
+  }
+
+  return errors.New("could not find corresponding type for unmarshalling string")
 }
 
 // NoElementsError indicates that a Pop was attempted on an empty queue
@@ -48,9 +57,147 @@ const maxRingBufferSize = 128
 
 // BPQWithCapacity creates a new bounded priority queue with the given capacity
 func BPQWithCapacity(capacity int, queueType QueueType) BPQ {
-  if capacity <= maxRingBufferSize {
-    return makeRingBuffer(capacity, queueType)
+  return makeRingBuffer(capacity, queueType)
+}
+
+type ringBufferEntry struct {
+  value    interface{}
+  priority float64
+  inUse    bool
+}
+
+type BPQ struct {
+  entries              []ringBufferEntry
+  startIndex, endIndex int
+  compareFunc          func(float64, float64) bool
+  queueType            QueueType
+}
+
+func (e ringBufferEntry) String() string {
+  if e.inUse {
+    return fmt.Sprintf("{ Value %v, Priority %v }", e.value, e.priority)
   } else {
-    return makeBoundedHeap(capacity, queueType)
+    return fmt.Sprintf("{}")
   }
+}
+
+func (bpq BPQ) String() string {
+  return fmt.Sprintf("BPQ Ring Buffer: Start %v, End %v, Entries: %v", bpq.startIndex, bpq.endIndex, bpq.entries)
+}
+
+func makeRingBuffer(capacity int, queueType QueueType) BPQ {
+  var fn func(float64, float64) bool
+  if queueType == MaxQueue {
+    fn = func(a float64, b float64) bool {
+      return a > b
+    }
+  } else {
+    fn = func(a float64, b float64) bool {
+      return a < b
+    }
+  }
+
+  result := BPQ{make([]ringBufferEntry, capacity),
+    0, 0, fn, queueType}
+
+  for i := 0; i < capacity; i++ {
+    result.entries[i] = ringBufferEntry{nil, 0, false}
+  }
+
+  return result
+}
+
+func (bpq BPQ) Capacity() int {
+  return len(bpq.entries)
+}
+
+func (bpq BPQ) Push(item interface{}, priority float64) bool {
+  //defer fmt.Printf("Post-Push: %v", bpq)
+
+  if bpq.entries[bpq.endIndex].inUse && bpq.compareFunc(bpq.entries[bpq.endIndex].priority, priority) {
+    // We can't insert
+    return false
+  }
+
+  index := bpq.endIndex
+  bpq.entries[index].value = item
+  bpq.entries[index].priority = priority
+  bpq.entries[index].inUse = true
+
+  nextIndex := (bpq.endIndex + 1) % len(bpq.entries)
+  if nextIndex != bpq.startIndex {
+    bpq.endIndex = nextIndex
+  }
+
+  // Pull it backwards until it's at the right place
+  for index != bpq.startIndex && bpq.entries[index].inUse {
+    var prevIndex int
+    if index == 0 {
+      prevIndex = len(bpq.entries) - 1
+    } else {
+      prevIndex = index - 1
+    }
+
+    if bpq.compareFunc(bpq.entries[index].priority, bpq.entries[prevIndex].priority) {
+      bpq.entries[prevIndex], bpq.entries[index] = bpq.entries[index], bpq.entries[prevIndex]
+
+      index = prevIndex
+    } else {
+      break
+    }
+  }
+
+  return true
+}
+
+func (bpq BPQ) Pop() (QueueItem, error) {
+  if bpq.entries[bpq.startIndex].inUse == false {
+    return QueueItem{}, NoElementsError
+  }
+
+  result := QueueItem{
+    Value:    bpq.entries[bpq.startIndex].value,
+    Priority: bpq.entries[bpq.startIndex].priority,
+  }
+  bpq.entries[bpq.startIndex].inUse = false
+
+  bpq.startIndex = bpq.startIndex + 1
+  if bpq.startIndex == len(bpq.entries) {
+    bpq.startIndex = 0
+  }
+
+  return result, nil
+}
+
+func (bpq BPQ) MarshalJSON() ([]byte, error) {
+  buffer := make([]QueueItem, 0, 0)
+  for {
+    item, err := bpq.Pop()
+    if err != nil {
+      break
+    }
+
+    buffer = append(buffer, item)
+  }
+
+  return json.Marshal(QueueMarshalled{
+    Items:    buffer,
+    Capacity: cap(bpq.entries),
+    Ordering: bpq.queueType,
+  })
+}
+
+func (bpq *BPQ) UnmarshalJSON(data []byte) error {
+  var buffer QueueMarshalled
+  if err := json.Unmarshal(data, &buffer); err != nil {
+    return err
+  }
+
+  *bpq = BPQWithCapacity(buffer.Capacity, buffer.Ordering)
+
+  for _, item := range buffer.Items {
+    bpq.Push(item.Value, item.Priority)
+  }
+
+  return nil
 }
